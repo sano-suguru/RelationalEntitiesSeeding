2e543848c371d6a11486dde81d5ec5d4785171ef
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-28 00:09:17 +0900

    Authorエンティティを実装

---
+++ RelationalEntitiesSeeding/Entities/Author.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RelationalEntitiesSeeding.Entities {
+  public class Author {
+    public int AuthorId { get; set; }
+    public string Name { get; set; }
+    public DateTime Birthday { get; set; }
+    public IList<Book> Books { get; set; }
+  }
+}
