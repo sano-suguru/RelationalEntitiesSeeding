e91c368c872f908eecfca0f413b5c6db14556bc1
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-28 00:47:24 +0900

    DB制約と初期データの投入処理を実装

--- RelationalEntitiesSeeding/Data/MyDbContext.cs
+++ RelationalEntitiesSeeding/Data/MyDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RelationalEntitiesSeeding.Entities;
+using System;
 
 namespace RelationalEntitiesSeeding.Data {
   public class MyDbContext : DbContext {
@@ -8,5 +9,27 @@ namespace RelationalEntitiesSeeding.Data {
 
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.Entity<Author>(entity => {
+        entity.Property(author => author.Name).IsRequired().HasMaxLength(1000);
+        entity.HasMany(author => author.Books).WithOne(book => book.Author);
+        entity.HasData(
+          new Author { AuthorId = 1, Name = "フィリップ・K・ディック", Birthday = DateTime.Parse("1928/1216") },
+          new Author { AuthorId = 2, Name = "ジョージ・オーウェル", Birthday = DateTime.Parse("1903/06/25") }
+        );
+      });
+
+
+      modelBuilder.Entity<Book>(entity => {
+        entity.Property(book => book.Title).IsRequired().HasMaxLength(1000);
+        entity.HasOne(book => book.Author).WithMany(author => author.Books);
+        entity.HasData(
+          new Book { BookId = 1, Title = "アンドロイドは電気羊の夢を見るか?", PublushYear = 1969, AuthorId = 1 },
+          new Book { BookId = 2, Title = "高い城の男", PublushYear = 1962, AuthorId = 1 },
+          new Book { BookId = 3, Title = "1984年", PublushYear = 1949, AuthorId = 2 }
+        );
+      });
+    }
   }
 }
