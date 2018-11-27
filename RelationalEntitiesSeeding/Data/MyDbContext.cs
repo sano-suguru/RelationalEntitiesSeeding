d6d8a8ce4071192646fd38f9fdbd0c6375f1f4fc
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-28 00:45:32 +0900

    データベースコンテキストを実装

---
+++ RelationalEntitiesSeeding/Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RelationalEntitiesSeeding.Entities;
+
+namespace RelationalEntitiesSeeding.Data {
+  public class MyDbContext : DbContext {
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+      : base(options) { }
+
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+  }
+}
