4d48390b020d530fcbde916cd97e481691987803
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-28 01:52:55 +0900

    AuhtorsControllerを作成し一覧取得を実装

---
+++ RelationalEntitiesSeeding/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RelationalEntitiesSeeding.Data;
+using RelationalEntitiesSeeding.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RelationalEntitiesSeeding.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthorsController : ControllerBase {
+    private readonly MyDbContext context;
+
+    public AuthorsController(MyDbContext context) {
+      this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get() =>
+      Ok(await context.Authors
+        .Include(a => a.Books)
+        .ToListAsync()
+      );
+  }
+}
