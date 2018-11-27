7a5ef955606353a53c6623d8d1748b55c4057079
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-28 00:09:39 +0900

    Bookエンティティ実装

---
+++ RelationalEntitiesSeeding/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace RelationalEntitiesSeeding.Entities {
+  public class Book {
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public int PublushYear { get; set; }
+
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
+  }
+}
